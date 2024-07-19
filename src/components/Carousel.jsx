461df4c9741d1e99ef242e969d0c3fb492d22979
Peter <Peter_Sadek@outlook.com>
2024-07-19 18:41:11 +0300

    Created Carousel as a class component

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,35 @@
+import { Component } from 'react';
+
+class Carousel extends Component {
+  state = {
+    active: 0
+  };
+
+  static defaultProps = {
+    images: ['http://pets-images.dev-apis.com/pets/none.jpg']
+  };
+  render() {
+    this.props;
+    const { images } = this.props;
+    const { active } = this.state;
+
+    return (
+      <div className='carousel'>
+        <img src={images[active]} alt='animal' />
+        <div className='carousel-smaller'>
+          {images.map((image, index) => (
+            <img
+              key={image}
+              src={image}
+              alt='animal thumbnail'
+              className={active == index ? 'active' : ''}
+              onClick={() => this.setState({ active: index })}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import fetchPetDetails from '../apis/fetchPetDetails';
+import Carousel from './Carousel';
 
 const Details = () => {
   const { id } = useParams();
@@ -10,15 +11,15 @@ const Details = () => {
   if (result.isLoading)
     return (
       <span className='loading-pane'>
-        <h2 className='loader'>🐢</h2>
+        <h2 className='loader'>⚙</h2>
       </span>
     );
 
   const pet = result.data.pets[0];
   return (
     <div className='details'>
+      <Carousel images={pet.images} id={pet.id} />
       <div>
-        {' '}
         <h1>{pet.name}</h1>
         <h2>
           <span className='capitalize'>{pet.animal} </span>- {pet.breed} -{' '}
